62bca2b61de8316fb50e658156375f0e62ccc6a8
VoTanSang22 <107173153+VoTanSang22@users.noreply.github.com>
2022-08-08 10:43:23 +0700

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace thegioididong.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
